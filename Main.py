8e8d5a152fb8331bdb1cd4c4980347d6ff7498ac
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2021-06-08 20:34:16 +0800

    初始化存储库。

---
+++ Main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :Main.py
+# @Time      :2021/6/8 20:32
+# @Author    :Amundsen Severus Rubeus Bjaaland
+
+
+if __name__ == "__main__":
+    run_code = 0
